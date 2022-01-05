615b8dc9dddf66ee54cb7633e83facd3570eb7a7
Zimtbieber <lukas.karle@t-online.de>
2022-01-05 16:58:46 +0100

    Working Webcam

--- TestOpenCV/main.cpp
+++ TestOpenCV/main.cpp
@@ -13,6 +13,8 @@
 using namespace cv;
 using namespace std;
 
+// ------------------------------------------------- READ IMAGES
+/*
 int main(int argc, const char * argv[]) {
     
     string path = "/Users/lukaskarle/Dev/C++/OpenCV/TestOpenCV/TestOpenCV/assets/rabbit.jpeg";
@@ -24,7 +26,48 @@ int main(int argc, const char * argv[]) {
     }
     
     imshow("Image", img);
-    waitKey(0);
+    waitKey(0); // infinity
     
+    return 0;
+}
+ */
+
+// ------------------------------------------------- READ VIDEO
+/*
+int main(int argc, const char * argv[]) {
+    
+    string path = "/Users/lukaskarle/Dev/C++/OpenCV/TestOpenCV/TestOpenCV/assets/BewegtBild_Uebung3.mp4";
+    VideoCapture cap(path);
+    
+    Mat img;
+    
+    while (true) {
+        cap.read(img);
+        imshow("Image", img);
+        waitKey(20); // 20 ms
+    }
+    
+    return 0;
+}
+*/
+
+// ------------------------------------------------- READ WEBCAM
+int main(int argc, const char * argv[]) {
+    
+    VideoCapture videoCapture(0);
+    
+    if (!videoCapture.isOpened()) {
+        cout << "Unable to connect to webcam"  << endl;
+        return -1;
+    }
+    
+    while(true) {
+        Mat frame;
+        videoCapture >> frame;
+        if(frame.empty()) break;
+        imshow("Camera feed", frame);
+        if (waitKey(10) == 27) break;
+    }
+    videoCapture.release();
     return 0;
 }
